2df0da7d8980656800cf2366fecd88ab20ab4651
Stephen Mathieson <smath23@gmail.com>
2013-11-25 23:31:04 -0500

    verify keywords and source count

--- test/package.c
+++ test/package.c
@@ -9,7 +9,6 @@
 #include "package.h"
 
 
-
 void test_package_from_json() {
   char json[] = "{"
                 "  \"name\": \"foo\","
@@ -52,25 +51,28 @@ void test_package_from_repo() {
   assert(0 == strcmp("String case conversion utility", pkg->description));
   assert(0 == strcmp("src/case.c", json_array_get_string(pkg->src, 0)));
   assert(0 == strcmp("src/case.h", json_array_get_string(pkg->src, 1)));
+  assert(2 == json_array_get_count(pkg->src));
+  assert(0 == strcmp("string", json_array_get_string(pkg->keywords, 0)));
+  assert(0 == strcmp("uppercase", json_array_get_string(pkg->keywords, 1)));
+  assert(0 == strcmp("lowercase", json_array_get_string(pkg->keywords, 2)));
+  assert(3 == json_array_get_count(pkg->keywords));
   free(pkg);
 }
 
 void test_package_install() {
   package_t *pkg = package_from_repo("clibs/buffer", "0.2.0");
   assert(pkg);
-
   assert(0 == package_install(pkg, "./test/fixtures"));
   assert(0 == fs_exists("./test/fixtures/package.json"));
   assert(0 == fs_exists("./test/fixtures/buffer.c"));
   assert(0 == fs_exists("./test/fixtures/buffer.h"));
+  free(pkg);
 }
 
 int main() {
   test_package_from_json();
   test_package_url();
   test_package_from_repo();
-
   test_package_install();
-
   return 0;
 }
